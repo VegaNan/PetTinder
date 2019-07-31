d56f54ae1d367f1ec7363658069aa1fec6da5b5f
VegaNan <47089431+VegaNan@users.noreply.github.com>
2019-07-31 09:10:00 -0600

    New important classes and packages for organization

---
+++ PetTinder/src/main/java/views/GUI.java
@@ -0,0 +1,18 @@
+package views;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application{
+
+	public static void main(String[] args) {
+		launch();
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
