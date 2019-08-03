f5c3fbcd4b7399b7debe9f71cc9bf9ec565f21a3
VegaNan <47089431+VegaNan@users.noreply.github.com>
2019-08-02 19:28:29 -0600

    functional API!

--- Pinder/src/main/java/controllers/DatabaseController.java
+++ Pinder/src/main/java/controllers/DatabaseController.java
@@ -1,5 +1,49 @@
 package controllers;
 
-public class DatabaseController {
+import java.net.UnknownHostException;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.util.JSON;
+
+public class DatabaseController {
+	private static DB database;
+	private static DBCollection collection;
+	
+	public DatabaseController() {
+		try {
+			String databaseName = "Pinder";
+			String collectionName = "APIInfo";
+			MongoClient mongoClient;
+			mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+			
+			System.out.println("created client");
+			database = mongoClient.getDB(databaseName);
+			System.out.println("got database");
+			collection = database.getCollection(collectionName);		
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+
+	public void insertAnimalRecords(String recordInfo) {
+		System.out.println("inserting record");
+		JSONObject obj = new JSONObject(recordInfo);
+		JSONArray jsonArr = obj.getJSONArray("animals");
+		for(int i = 0; i < jsonArr.length(); i++) {
+			DBObject dbObject = (DBObject) JSON.parse(jsonArr.getJSONObject(i).toString(1));
+			System.out.println(dbObject.toString());
+			collection.insert(dbObject);
+		}
+		
+	}
+	
+	
 }
