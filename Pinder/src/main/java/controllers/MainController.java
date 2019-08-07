4e5df72e79b5c5b8f56526260601086e7a057d5d
Jacob Nuttall <jnuttall164@gmail.com>
2019-08-07 09:15:05 -0600

    Update MainController.java
    Just some test stuff

--- Pinder/src/main/java/controllers/MainController.java
+++ Pinder/src/main/java/controllers/MainController.java
@@ -3,24 +3,15 @@ package controllers;
 public class MainController {
 
 	public static void main(String[] args) {
-		DatabaseController dc = new DatabaseController();
-		APIController.GetAccessToken();
-//		String info = APIController.animalRequest();
-//		dc.insertAnimalRecords(info);
-//		System.out.println("Done");
-//		System.out.println(dc.getAnimalById(45189636));
-		APIController.organizationRequest();
-		System.out.println(dc.getOrganizationById("UT189"));
-
 		DatabaseController dbController = new DatabaseController();
-		//APIController.GetAccessToken();
-		//String info = APIController.animalRequest();
-		//dbController.insertAnimalRecords(info);
-		//info = APIController.organizationRequest();
-		//dbController.insertOrganizationRecords(info);
-		System.out.println("org by id   " + dbController.getOrganizationById("UT189"));
-		System.out.println("animal by org   " + dbController.getAnimalsByOrganization("UT189"));
-		System.out.println("animal by org val   " + dbController.getAnimalsByOrganizationValue("postcode", "84660"));
-
+//		APIController.GetAccessToken();
+//		String info = APIController.animalRequest();
+//		dbController.insertAnimalRecords(info);
+//		info = APIController.organizationRequest();
+//		dbController.insertOrganizationRecords(info);
+//		System.out.println("org by id   " + dbController.getOrganizationById("UT189"));
+//		System.out.println("animal by org   " + dbController.getAnimalsByOrganization("UT189"));
+//		System.out.println("animal by org val   " + dbController.getAnimalsByOrganizationValue("address.postcode", "84660"));
+//		System.out.println("animal by val	" + dbController.searchAnimalsBy("status", "adoptable"));
 	}
 }
