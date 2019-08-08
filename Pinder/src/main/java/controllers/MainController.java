10cde8bb8c450e845e4c5dc383ee4add97c9ae1e
Beck <bes6896@yahoo.com>
2019-08-08 09:12:59 -0600

    Comment Out

--- Pinder/src/main/java/controllers/MainController.java
+++ Pinder/src/main/java/controllers/MainController.java
@@ -4,41 +4,37 @@ package controllers;
 public class MainController {
 
 	static DatabaseController dbController = new DatabaseController();
-	
-	/*		
-	 	attributes.spayed_neutered
-		status
-		size
-		type
-		age
-		distance
-	*/
-	
+
 	/*
-		Baby
-		Young
-		Adult
-		Senior
-	*/
+	 * attributes.spayed_neutered status size type age distance
+	 */
+
+	/*
+	 * Baby Young Adult Senior
+	 */
 
 	public static void main(String[] args) {
-		/*APIController.GetAccessToken();
-		String info = APIController.animalRequest();
-		dbController.insertAnimalRecords(info);
-		info = APIController.organizationRequest();
-		dbController.insertOrganizationRecords(info);
-		System.out.println("org by id   " + dbController.getOrganizationById("UT202"));
-		System.out.println("animal by org   " + dbController.getAnimalsByOrganization("UT202"));
-		System.out.println("animal by org val   " + dbController.getAnimalsByOrganizationValue("address.postcode", "84660"));
-		System.out.println("test animals by   " + dbController.getAnimalsBy("gender", "Female"));
-
-		System.out.println("test pet search   " + petSearch("gender", "Female"));
-
+		/*
+		 * APIController.GetAccessToken(); String info = APIController.animalRequest();
+		 * dbController.insertAnimalRecords(info); info =
+		 * APIController.organizationRequest();
+		 * dbController.insertOrganizationRecords(info);
+		 * System.out.println("org by id   " +
+		 * dbController.getOrganizationById("UT202"));
+		 * System.out.println("animal by org   " +
+		 * dbController.getAnimalsByOrganization("UT202"));
+		 * System.out.println("animal by org val   " +
+		 * dbController.getAnimalsByOrganizationValue("address.postcode", "84660"));
+		 * System.out.println("test animals by   " + dbController.getAnimalsBy("gender",
+		 * "Female"));
+		 * 
+		 * System.out.println("test pet search   " + petSearch("gender", "Female"));
+		 */
 	}
-	
+
 	private static String petSearch(String petInfo, String value) {
 		String results = dbController.getAnimalsBy(petInfo, value);
 		return results;
 	}
-	
+
 }
