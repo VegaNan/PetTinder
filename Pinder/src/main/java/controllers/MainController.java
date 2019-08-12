5e0644d6a0efd5f0f6dd1f0d8be66f9144a80249
Beck <bes6896@yahoo.com>
2019-08-12 08:29:42 -0600

    ?

--- Pinder/src/main/java/controllers/MainController.java
+++ Pinder/src/main/java/controllers/MainController.java
@@ -17,18 +17,17 @@ public class MainController {
 	}
 
 	public static void main(String[] args) {
-		//APIController.GetAccessToken();
-		//String info = APIController.animalRequest();
-		//dbController.insertAnimalRecords(info);
-		//info = APIController.organizationRequest();
-		//dbController.insertOrganizationRecords(info);
+		APIController.GetAccessToken();
+		String info = APIController.animalRequest();
+		dbController.insertAnimalRecords(info);
+		info = APIController.organizationRequest();
+		dbController.insertOrganizationRecords(info);
 		/*
 		System.out.println("org by id   " + dbController.getOrganizationById("UT202"));
 		System.out.println("animal by org   " + dbController.getAnimalsByOrganization("UT202"));
 		System.out.println("animal by org val   " + dbController.getAnimalsByOrganizationValue("address.postcode", "84660"));
 		System.out.println("test animals by   " + dbController.getAnimalsBy("gender", "Female"));
 		*/
-		petSearch(petInfo.SpayedNeutered, "false");
 	}
 	
 	private static String petSearch(petInfo petinfo, String value) {
