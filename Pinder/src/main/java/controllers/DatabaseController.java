8e330e757f9c0ee9c95f3205d35104ef23d59109
Beck <bes6896@yahoo.com>
2019-08-15 09:41:15 -0600

    More Info Controller Stuff (Not Much Yet)

--- Pinder/src/main/java/controllers/DatabaseController.java
+++ Pinder/src/main/java/controllers/DatabaseController.java
@@ -1,4 +1,5 @@
 package controllers;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +25,7 @@ public class DatabaseController {
 	private static MongoCollection<Document> userCollection;
 	private String databaseName;
 	private MongoClient mongoClient;
-	
+
 	public DatabaseController() {
 		databaseName = "Pinder";
 		mongoClient = new MongoClient("localhost", 27017);
@@ -39,37 +40,37 @@ public class DatabaseController {
 		System.out.println("inserting record page");
 		JSONObject obj = new JSONObject(recordInfo);
 		JSONArray jsonArr = obj.getJSONArray("animals");
-		for(int i = 0; i < jsonArr.length(); i++) {
+		for (int i = 0; i < jsonArr.length(); i++) {
 			Document dbObject = Document.parse(jsonArr.getJSONObject(i).toString(1));
-			if(dbObject != null) {
+			if (dbObject != null) {
 				animalCollection.insertOne(dbObject);
 			}
 		}
 	}
-	
+
 	public void insertOrganizationRecords(String recordInfo) {
 		System.out.println("inserting record page");
 		JSONObject obj = new JSONObject(recordInfo);
 		JSONArray jsonArr = obj.getJSONArray("organizations");
-		for(int i = 0; i < jsonArr.length(); i++) {
+		for (int i = 0; i < jsonArr.length(); i++) {
 			Document dbObject = Document.parse(jsonArr.getJSONObject(i).toString(1));
-			if(dbObject != null) {
+			if (dbObject != null) {
 				organizationCollection.insertOne(dbObject);
 			}
 		}
 	}
-	
+
 	public Animal getAnimalById(int id) {
 		MongoCollection<Document> collectionResults = animalCollection;
 		BasicDBObject fields = new BasicDBObject();
 		fields.put("id", id);
 		FindIterable<Document> cursor = collectionResults.find(fields);
 		String dbString = cursor.first().toString();
-		
+
 		Animal animal = createAnimalObjects(dbString)[0];
 		return animal;
 	}
-	
+
 	public Organization getOrganizationById(String id) {
 		MongoCollection<Document> collectionResults = organizationCollection;
 		BasicDBObject fields = new BasicDBObject();
@@ -79,131 +80,131 @@ public class DatabaseController {
 		Organization org = createOrganizationObjects(dbString)[0];
 		return org;
 	}
-	
+
 	public Animal[] getAnimalsByOrganization(String id) {
 		String animals = "";
 		MongoCollection<Document> collectionResults = animalCollection;
 		BasicDBObject fields = new BasicDBObject();
 		fields.put("organization_id", id);
 		Iterable<Document> cursor = collectionResults.find(fields);
-		for(Document doc: cursor) {
-			animals+=doc.toJson();
-			animals+=doc.toJson() + "~";
+		for (Document doc : cursor) {
+			animals += doc.toJson();
+			animals += doc.toJson() + "~";
 
 		}
 		Animal[] animalArr = createAnimalObjects(animals);
 		return animalArr;
 	}
-	
+
 	public Animal[] getAnimalsBy(String key, String value) {
 		String animals = "";
 		MongoCollection<Document> collectionResults = animalCollection;
 		BasicDBObject fields = new BasicDBObject();
 		fields.put(key, value);
 		Iterable<Document> cursor = collectionResults.find(fields);
-		for(Document doc: cursor) {
+		for (Document doc : cursor) {
 
 			System.out.println(doc.toString());
-			animals+=doc.toJson() + "~";
+			animals += doc.toJson() + "~";
 
 		}
 		Animal[] animalArr = createAnimalObjects(animals);
 		return animalArr;
 	}
-	
+
 	public Animal[] getAnimalsByOrganizationValue(String orgKey, String orgValue) {
 
 		MongoCollection<Document> organizationResults = organizationCollection;
 		BasicDBObject fields = new BasicDBObject();
 		fields.put(orgKey, orgValue);
 		Iterable<Document> cursor = organizationResults.find(fields);
-		
+
 		String animals = "";
-		
-		for(Document doc: cursor) {
+
+		for (Document doc : cursor) {
 			String id = doc.getString("id");
 			System.out.println(id);
 			MongoCollection<Document> animalResults = animalCollection;
 			BasicDBObject fields2 = new BasicDBObject();
 			fields2.put("organization_id", id);
 			Iterable<Document> cursor2 = animalResults.find(fields2);
-			for(Document doc2: cursor2) {
-				animals+=doc2.toJson();
+			for (Document doc2 : cursor2) {
+				animals += doc2.toJson();
 			}
 		}
-		
-		Animal[] animalsObj =  createAnimalObjects(animals);
+
+		Animal[] animalsObj = createAnimalObjects(animals);
 		return animalsObj;
 	}
-	
+
 	public Animal[] createAnimalObjects(String dbString) {
 		String[] dbAnimals = dbString.split("~");
 		System.out.println(dbString);
-		
+
 		int animalNum = dbAnimals.length;
 		Animal[] animals = new Animal[animalNum];
-		
-		for(int i = 0; i < animalNum; i++) {
+
+		for (int i = 0; i < animalNum; i++) {
 			System.out.println(dbAnimals[i]);
 			JSONObject jo = new JSONObject(dbAnimals[i]);
 
 			int id = Integer.parseInt(jo.get("id").toString());
 			String organizationId = jo.getString("organization_id");
-			String type = jo.getString("type"); 
-			String breed = jo.getJSONObject("breeds").getString("primary"); 
-			String size =  jo.getString("size"); 
-			String gender =  jo.getString("gender");
-			String age =  jo.getString("age");
-			String status =  jo.getString("status");
-			String name =  jo.getString("name");
+			String type = jo.getString("type");
+			String breed = jo.getJSONObject("breeds").getString("primary");
+			String size = jo.getString("size");
+			String gender = jo.getString("gender");
+			String age = jo.getString("age");
+			String status = jo.getString("status");
+			String name = jo.getString("name");
 			String organization = jo.getString("organization_id");
-			boolean goodWithChildren = Boolean.parseBoolean( jo.getJSONObject("environment").get("children").toString()); 
-			boolean goodWithDogs = Boolean.parseBoolean( jo.getJSONObject("environment").get("dogs").toString());
-			boolean goodWithCats = Boolean.parseBoolean( jo.getJSONObject("environment").get("cats").toString()); 
-			String location =  jo.getJSONObject("contact").getJSONObject("address").getString("city") + jo.getJSONObject("contact").getJSONObject("address").getString("state") +  jo.getJSONObject("contact").getJSONObject("address").getString("postcode")  ; 
-			double distance = Double.parseDouble( jo.get("distance").toString()); 
-			boolean spayedNeutered = Boolean.parseBoolean( jo.getJSONObject("attributes").get("spayed_neutered").toString()); 
-			boolean houseTrained = Boolean.parseBoolean( jo.getJSONObject("attributes").get("house_trained").toString());
-			boolean declawed = Boolean.parseBoolean( jo.getJSONObject("attributes").get("declawed").toString()); 
-			
+			boolean goodWithChildren = Boolean.parseBoolean(jo.getJSONObject("environment").get("children").toString());
+			boolean goodWithDogs = Boolean.parseBoolean(jo.getJSONObject("environment").get("dogs").toString());
+			boolean goodWithCats = Boolean.parseBoolean(jo.getJSONObject("environment").get("cats").toString());
+			String location = jo.getJSONObject("contact").getJSONObject("address").getString("city")
+					+ jo.getJSONObject("contact").getJSONObject("address").getString("state")
+					+ jo.getJSONObject("contact").getJSONObject("address").getString("postcode");
+			double distance = Double.parseDouble(jo.get("distance").toString());
+			boolean spayedNeutered = Boolean
+					.parseBoolean(jo.getJSONObject("attributes").get("spayed_neutered").toString());
+			boolean houseTrained = Boolean.parseBoolean(jo.getJSONObject("attributes").get("house_trained").toString());
+			boolean declawed = Boolean.parseBoolean(jo.getJSONObject("attributes").get("declawed").toString());
+
 			String description = "";
-			if(jo.get("description").toString() != null){
+			if (jo.get("description").toString() != null) {
 				description = jo.get("description").toString();
 			}
 
-			
 			String photosUrl[] = new String[jo.getJSONArray("photos").length()];
-			if(!jo.getJSONArray("photos").isEmpty()) {	
-				for(int j = 0; j < jo.getJSONArray("photos").length(); j++) {
+			if (!jo.getJSONArray("photos").isEmpty()) {
+				for (int j = 0; j < jo.getJSONArray("photos").length(); j++) {
 					photosUrl[j] = jo.getJSONArray("photos").getJSONObject(j).getString("full");
 				}
-			}else {
+			} else {
 				photosUrl = new String[1];
 				photosUrl[0] = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png";
 			}
 			ArrayList<String> tags = null;
-	
-			animals[i] = new Animal(id, organizationId, type, breed, size, gender, age,
-					status, name, organization, goodWithChildren, goodWithDogs,
-					goodWithCats, location, distance, spayedNeutered, houseTrained,
+
+			animals[i] = new Animal(id, organizationId, type, breed, size, gender, age, status, name, organization,
+					goodWithChildren, goodWithDogs, goodWithCats, location, distance, spayedNeutered, houseTrained,
 					declawed, photosUrl, tags, description, dbString);
-			}
+		}
 		return animals;
 	}
 
-	public Organization[] createOrganizationObjects(String dbString){
+	public Organization[] createOrganizationObjects(String dbString) {
 		String[] dbOrgs = dbString.split("~");
 		System.out.println(dbString);
-		
+
 		int dbLength = dbOrgs.length;
 		Organization[] orgArr = new Organization[dbLength];
-		
-		
-		for(int i = 0; i < dbLength; i++) {
+
+		for (int i = 0; i < dbLength; i++) {
 			System.out.println(dbOrgs[i]);
 			JSONObject jo = new JSONObject(dbOrgs[i]);
 
-			String organizationId = jo.getString("id"); 
+			String organizationId = jo.getString("id");
 			String name = jo.getString("name");
 			String location = jo.getString("address1");
 			String state = jo.getString("state");
@@ -212,125 +213,118 @@ public class DatabaseController {
 			String contactEmail = jo.getString("email");
 			String contactPhone = jo.getString("phone");
 			String websiteUrl = jo.getString("website");
-	
-			orgArr[i] = new Organization(organizationId, name, location, state, country, contactEmail, contactPhone, zipcode, websiteUrl);
+
+			orgArr[i] = new Organization(organizationId, name, location, state, country, contactEmail, contactPhone,
+					zipcode, websiteUrl);
 		}
-		
+
 		return orgArr;
 	}
-	
+
 	public User userLogin(String username, String password) {
 		User user = new User();
-		
-		
+
 		return user;
 	}
-	
+
 	public void storeUser(User user) {
-		Document document = new Document("firstName", user.getFirstName())
-				.append("lastName", user.getLastName())
-				.append("password", user.getPassword())
-				.append("location", user.getLocation())
-				.append("email", user.getEmail())
-				.append("pref", user.getAnimalPref());
-		
+		Document document = new Document("firstName", user.getFirstName()).append("lastName", user.getLastName())
+				.append("password", user.getPassword()).append("location", user.getLocation())
+				.append("email", user.getEmail()).append("pref", user.getAnimalPref());
+
 		List<Document> matchedArr = new ArrayList<>();
 		List<Document> maybeArr = new ArrayList<>();
 		List<Document> noArr = new ArrayList<>();
-		
-		for(Object animal : user.getMatched().toArray()) {
+
+		for (Object animal : user.getMatched().toArray()) {
 			Document documentMatched = new Document();
-			documentMatched.append("\""  + "\"", (Object)animal);
+			documentMatched.append("\"" + "\"", (Object) animal);
 			matchedArr.add(documentMatched);
 		}
-		
+
 		document.append("matchedMap", matchedArr);
-		
-		for(Map.Entry<Integer, String> maybeMap : user.getMaybeMap().entrySet()) {
+
+		for (Map.Entry<Integer, String> maybeMap : user.getMaybeMap().entrySet()) {
 			Document documentMaybe = new Document();
-			documentMaybe.append("\"" + maybeMap.getKey() + "\"", (Object)maybeMap.getValue());
+			documentMaybe.append("\"" + maybeMap.getKey() + "\"", (Object) maybeMap.getValue());
 			maybeArr.add(documentMaybe);
 		}
-		
+
 		document.append("maybeMap", maybeArr);
 
-		for(Map.Entry<Integer, String> noMap : user.getNoMap().entrySet()) {
+		for (Map.Entry<Integer, String> noMap : user.getNoMap().entrySet()) {
 			Document documentNo = new Document();
-			documentNo.append("\"" + noMap.getKey() + "\"", (Object)noMap.getValue());
+			documentNo.append("\"" + noMap.getKey() + "\"", (Object) noMap.getValue());
 			noArr.add(documentNo);
 		}
 
 		document.append("noMap", noArr);
-		
+
 		userCollection.insertOne(document);
-		System.out.println("Document inserted successfully"); 
+		System.out.println("Document inserted successfully");
 	}
-	
+
 	public void updateUser(User user) {
 		BasicDBObject documentDelete = new BasicDBObject();
 		documentDelete.put("email", user.getEmail());
 		userCollection.deleteOne(documentDelete);
-		
-		Document document = new Document("firstName", user.getFirstName())
-				.append("lastName", user.getLastName())
-				.append("password", user.getPassword())
-				.append("location", user.getLocation())
-				.append("email", user.getEmail())
-				.append("pref", user.getAnimalPref());
-		
+
+		Document document = new Document("firstName", user.getFirstName()).append("lastName", user.getLastName())
+				.append("password", user.getPassword()).append("location", user.getLocation())
+				.append("email", user.getEmail()).append("pref", user.getAnimalPref());
+
 		List<Document> matchedArr = new ArrayList<>();
 		List<Document> maybeArr = new ArrayList<>();
 		List<Document> noArr = new ArrayList<>();
 		Animal[] animals = new Animal[user.getMatched().size()];
 		user.getMatched().toArray(animals);
-		for(Animal animal : animals) {
+		for (Animal animal : animals) {
 			Document documentMatched = new Document();
-			documentMatched.append("\""+ "\"", animal.getDbString());
+			documentMatched.append("\"" + "\"", animal.getDbString());
 			matchedArr.add(documentMatched);
 		}
-		
+
 		document.append("matchedMap", matchedArr);
-		
-		for(Map.Entry<Integer, String> maybeMap : user.getMaybeMap().entrySet()) {
+
+		for (Map.Entry<Integer, String> maybeMap : user.getMaybeMap().entrySet()) {
 			Document documentMaybe = new Document();
-			documentMaybe.append("\"" + maybeMap.getKey() + "\"", (Object)maybeMap.getValue());
+			documentMaybe.append("\"" + maybeMap.getKey() + "\"", (Object) maybeMap.getValue());
 			maybeArr.add(documentMaybe);
 		}
 
 		document.append("maybeMap", maybeArr);
-		
-		for(Map.Entry<Integer, String> noMap : user.getNoMap().entrySet()) {
+
+		for (Map.Entry<Integer, String> noMap : user.getNoMap().entrySet()) {
 			Document documentNo = new Document();
-			documentNo.append("\"" + noMap.getKey() + "\"", (Object)noMap.getValue());
+			documentNo.append("\"" + noMap.getKey() + "\"", (Object) noMap.getValue());
 			noArr.add(documentNo);
 		}
 
-		
 		document.append("noMap", noArr);
-		
+
 		userCollection.insertOne(document);
-		System.out.println("Document inserted successfully");	
+		System.out.println("Document inserted successfully");
 
 	}
-	
+
 	public User getUser(String email, String password) {
 		User user = null;
 		String correctPassword = null;
-		
+
 		MongoCollection<Document> userResults = userCollection;
 		BasicDBObject fields = new BasicDBObject();
 		fields.put("email", email);
 		Iterable<Document> cursor = userResults.find(fields);
-		
-		for(Document doc : cursor) {
+
+		for (Document doc : cursor) {
 			correctPassword = (String) doc.get("password");
-			if(password.equals(correctPassword)) {
-				user = new User(doc.getString("firstName"), doc.getString("lastName"), doc.getString("password"), doc.getString("email"), doc.getString("location"));
+			if (password.equals(correctPassword)) {
+				user = new User(doc.getString("firstName"), doc.getString("lastName"), doc.getString("password"),
+						doc.getString("email"), doc.getString("location"));
 			}
 		}
-		
-		
+
 		return user;
 	}
-	
+
 }
--- Pinder/src/main/java/controllers/MoreInfoController.java
+++ Pinder/src/main/java/controllers/MoreInfoController.java
@@ -1,5 +1,10 @@
 package controllers;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import views.GUI;
 
@@ -7,5 +12,17 @@ public class MoreInfoController {
 
 	private Stage primaryStage = GUI.primaryStage;
 	DatabaseController dbc = new DatabaseController();
+
+	public void matchesPage() throws IOException {
+		Parent parent = FXMLLoader.load(getClass().getResource("/MoreInfoPage.fxml"));
+		Scene scene = new Scene(parent);
+		Stage window = primaryStage;
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/MoreInfoPage.fxml"));
+
+		window.setScene(scene);
+		window.show();
+
+	}
 
 }
