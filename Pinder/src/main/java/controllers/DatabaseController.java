53fd284c757c9d408ed2499ddfd42ffa58e2a483
Jacob Nuttall <jnuttall164@gmail.com>
2019-08-15 20:34:53 -0600

    USER PERSISTANCE
    It works hallelujah. Well works for me sorry if it breaks anything

--- Pinder/src/main/java/controllers/DatabaseController.java
+++ Pinder/src/main/java/controllers/DatabaseController.java
@@ -239,34 +239,28 @@ public class DatabaseController {
 				.append("email", user.getEmail())
 				.append("pref", user.getAnimalPref());
 		
-		List<Document> matchedArr = new ArrayList<>();
-		List<Document> maybeArr = new ArrayList<>();
-		List<Document> noArr = new ArrayList<>();
+		List<String> matchedArr = user.getMatched();
+		List<String> newMatchedArr = new ArrayList<>();
+		List<String> maybeArr = user.getMaybe();
+		List<String> newMaybeArr = new ArrayList<>();
+		List<String> noArr = user.getNo();
+		List<String> newNoArr = new ArrayList<>();
 		
-		for(Object animal : user.getMatched().toArray()) {
-			Document documentMatched = new Document();
-			documentMatched.append("\""  + "\"", (Object)animal);
-			matchedArr.add(documentMatched);
+		for(String animal : matchedArr) {
+			newMatchedArr.add(animal);
 		}
-		
-		document.append("matchedMap", matchedArr);
+		document.append("matchedArr", newMatchedArr);
 
-		for(Object animal : user.getMaybe().toArray()) {
-			Document documentMatched = new Document();
-			documentMatched.append("\""  + "\"", (Object)animal);
-			matchedArr.add(documentMatched);
+		for(String animal : maybeArr) {
+			newMaybeArr.add(animal);
 		}
-		
-		document.append("maybeMap", maybeArr);
+		document.append("maybeArr", newMaybeArr);
 
 
-		for(Object animal : user.getNo().toArray()) {
-			Document documentMatched = new Document();
-			documentMatched.append("\""  + "\"", (Object)animal);
-			matchedArr.add(documentMatched);
+		for(String animal : noArr) {
+			newNoArr.add(animal);
 		}
-
-		document.append("noMap", noArr);
+		document.append("noArr", noArr);
 		
 		userCollection.insertOne(document);
 		System.out.println("Document inserted successfully"); 
@@ -284,40 +278,29 @@ public class DatabaseController {
 				.append("email", user.getEmail())
 				.append("pref", user.getAnimalPref());
 		
-		List<Document> matchedArr = new ArrayList<>();
-		List<Document> maybeArr = new ArrayList<>();
-		List<Document> noArr = new ArrayList<>();
-		String[] animals = new String[user.getMatched().size()];
-		user.getMatched().toArray(animals);
-		for(String id : animals) {
-			Document documentMatched = new Document();
-			documentMatched.append("\"ID"+ "\"", id);
-			matchedArr.add(documentMatched);
-		}
+		List<String> matchedArr = user.getMatched();
+		List<String> newMatchedArr = new ArrayList<>();
+		List<String> maybeArr = user.getMaybe();
+		List<String> newMaybeArr = new ArrayList<>();
+		List<String> noArr = user.getNo();
+		List<String> newNoArr = new ArrayList<>();
 		
-		document.append("matchedMap", matchedArr);
 		
-
-		animals = new String[user.getMaybe().size()];
-		user.getMaybe().toArray(animals);
-		for(String id: animals) {
-			Document documentMaybe = new Document();
-			documentMaybe.append("\"ID"+ "\"", id);
-
-			maybeArr.add(documentMaybe);
+		for(String animal : matchedArr) {
+			newMatchedArr.add(animal);
 		}
+		document.append("matchedArr", newMatchedArr);
 
-		document.append("maybeMap", maybeArr);
-		
-
-		animals = new String[user.getNo().size()];
-		user.getNo().toArray(animals);
-		for(String id : animals) {
-			Document documentNo = new Document();
-			documentNo.append("\"ID"+ "\"", id);
-			noArr.add(documentNo);
+		for(String animal : maybeArr) {
+			newMaybeArr.add(animal);
 		}
-		document.append("noMap", noArr);
+		document.append("maybeArr", newMaybeArr);
+
+
+		for(String animal : noArr) {
+			newNoArr.add(animal);
+		}
+		document.append("noArr", noArr);
 		
 		userCollection.insertOne(document);
 		System.out.println("Document inserted successfully");	
@@ -336,74 +319,15 @@ public class DatabaseController {
 		for(Document doc : cursor) {
 			correctPassword = (String) doc.get("password");
 			if(password.equals(correctPassword)) {
-				user = new User(doc.getString("firstName"), doc.getString("lastName"), doc.getString("password"), doc.getString("email"), doc.getString("location"));
+				user = new User(doc.getString("firstName"), doc.getString("lastName"), doc.getString("password"), doc.getString("email"), doc.getString("location"), doc.getString("pref"));
 
-				ArrayList<String> matchedArr = (ArrayList<String>) doc.get("matchedMap");
-				createAnimalList(matchedArr);
-				user.setMatched(matchedArr);
-				
-				ArrayList<String> maybeArr = (ArrayList<String>) doc.get("maybeArr");
-				createAnimalList(maybeArr);
-				user.setMatched(maybeArr);
-				
-				ArrayList<String> noArr = (ArrayList<String>) doc.get("noArr");
-				createAnimalList(noArr);
-				user.setMatched(noArr);
-				
-//				for(Object animal : user.getMatched().toArray()) {
-//					Document documentMatched = new Document();
-//					documentMatched.append("\""  + "\"", (Object)animal);
-//					matchedArr.add(documentMatched);
-//				}
+				user.setMatched((ArrayList<String>) doc.get("matchedArr"));
+				user.setMaybe((ArrayList<String>) doc.get("maybeArr"));
+				user.setNo((ArrayList<String>) doc.get("noArr")); 
 			}
 		}
 		
 		return user;
-	}
-	
-	public ArrayList<String> createAnimalList(ArrayList<String> array){
-		final String idRegEx = "^\\{\"ID\"=([\\d]*)\\}$";
-		Pattern p = Pattern.compile(idRegEx);
-		System.out.println(array);
-		String result = "";
-//		for(int i = 0; i< array.size(); i++) {
-//			JSONObject jo = new JSONObject(array.get(i).toString());
-//			System.out.println(jo.toString());
-//		}
-		
-		System.out.println(result);
-		Matcher m = p.matcher(result);
-		
-		ArrayList<String> animals = new ArrayList<>();
-		while(m.find()) {
-			
-			animals.add(m.group(1));
-		}
-		
-//		System.out.println(dbAnimals[i]);
-//		JSONObject jo = new JSONObject(dbAnimals[i]);
-//
-//		String id = jo.get("id").toString();
-//		String organizationId = jo.getString("organization_id");
-//		String type = jo.getString("type"); 
-//		String breed = jo.getJSONObject("breeds").getString("primary"); 
-//		String size =  jo.getString("size"); 
-//		String gender =  jo.getString("gender");
-//		String age =  jo.getString("age");
-//		String status =  jo.getString("status");
-//		String name =  jo.getString("name");
-//		String organization = jo.getString("organization_id");
-//		boolean goodWithChildren = Boolean.parseBoolean( jo.getJSONObject("environment").get("children").toString()); 
-//		boolean goodWithDogs = Boolean.parseBoolean( jo.getJSONObject("environment").get("dogs").toString());
-//		boolean goodWithCats = Boolean.parseBoolean( jo.getJSONObject("environment").get("cats").toString()); 
-//		String location =  jo.getJSONObject("contact").getJSONObject("address").getString("city") + jo.getJSONObject("contact").getJSONObject("address").getString("state") +  jo.getJSONObject("contact").getJSONObject("address").getString("postcode")  ; 
-//		double distance = Double.parseDouble( jo.get("distance").toString()); 
-//		boolean spayedNeutered = Boolean.parseBoolean( jo.getJSONObject("attributes").get("spayed_neutered").toString()); 
-//		boolean houseTrained = Boolean.parseBoolean( jo.getJSONObject("attributes").get("house_trained").toString());
-//		boolean declawed = 
-		
-		
-		return animals;
 	}
 	
 }
--- Pinder/src/main/java/objects/User.java
+++ Pinder/src/main/java/objects/User.java
@@ -24,12 +24,13 @@ public class User {
 		
 	}
 	
-	public User(String firstName, String lastName, String password, String email, String location){
+	public User(String firstName, String lastName, String password, String email, String location, String pref){
 		setFirstName(firstName);
 		setLastName(lastName);
 		setPassword(password);
 		setEmail(email);
 		setLocation(location);
+		setAnimalPref(pref);
 	}
 	
 	public String getFirstName() {
