5233d039dde99012de8209d814fb842ffc38aa72
VegaNan <47089431+VegaNan@users.noreply.github.com>
2019-08-19 15:48:06 -0600

    more info looking better

--- Pinder/src/main/java/controllers/MoreInfoController.java
+++ Pinder/src/main/java/controllers/MoreInfoController.java
@@ -2,17 +2,37 @@ package controllers;
 
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import objects.Animal;
 import views.GUI;
 
 public class MoreInfoController {
 
 	private Stage primaryStage = GUI.primaryStage;
 	DatabaseController dbc = new DatabaseController();
-
+	@FXML
+	Label typeAnimalLabel, milesAwayLabel, organizationLabel, animalNameLabel;
+	@FXML
+	ImageView animalImage;
+	
+	@FXML
+	public void initialize() {
+		Animal animal = SwipeyPageController.currentAnimalArray[SwipeyPageController.inArraySlot];
+		typeAnimalLabel.setText("Type: "+ animal.getType());
+		milesAwayLabel.setText("Location: " + animal.getLocation());
+		organizationLabel.setText("Organization: " + animal.getOrganization());
+		animalNameLabel.setText(animal.getName());
+		animalImage.setImage(new Image(animal.getPhotosUrl()[0]));
+		
+	}
+	
 	public void matchesPage() throws IOException {
 		Parent parent = FXMLLoader.load(getClass().getResource("/MoreInfoPage.fxml"));
 		Scene scene = new Scene(parent);
