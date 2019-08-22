9611cadca367c90ee87d9fdf86fd997b0f1fc04f
VegaNan <47089431+VegaNan@users.noreply.github.com>
2019-08-22 09:13:45 -0600

    change button images

--- Pinder/src/main/java/controllers/MatchesPageController.java
+++ Pinder/src/main/java/controllers/MatchesPageController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -23,7 +24,7 @@ public class MatchesPageController {
 	private static String[] animalIds;
 
 	@FXML
-	ImageView petimg0, petimg1, petimg2, petimg3, petimg4, petimg5, petimg6, petimg7, petimg8, petimg9;
+	Button petimg0, petimg1, petimg2, petimg3, petimg4, petimg5, petimg6, petimg7, petimg8, petimg9;
 	@FXML
 	Label pettxt0, pettxt1, pettxt2, pettxt3, pettxt4, pettxt5, pettxt6, pettxt7, pettxt8, pettxt9;
 	@FXML
@@ -77,46 +78,47 @@ public class MatchesPageController {
 				Animal animal = dbc.getAnimalById(id);
 				String photo = animal.getPhotosUrl()[0];
 				String text = animal.getDescription();
-				Image photoImg = new Image(photo);
+				ImageView photoImg = new ImageView(new Image(photo));
+				
 				switch(slot){
 				case 0:
-					petimg0.setImage(photoImg);
+					petimg0.setGraphic(photoImg);
 					pettxt0.setText(text);
 					break;
 				case 1:
-					petimg1.setImage(photoImg);
+					petimg1.setGraphic(photoImg);
 					pettxt1.setText(animal.getDescription());
 					break;
 				case 2:
-					petimg2.setImage(photoImg);
+					petimg2.setGraphic(photoImg);
 					pettxt2.setText(animal.getDescription());
 					break;
 				case 3:
-					petimg3.setImage(photoImg);
+					petimg3.setGraphic(photoImg);
 					pettxt3.setText(animal.getDescription());
 					break;
 				case 4:		
-					petimg4.setImage(photoImg);
+					petimg4.setGraphic(photoImg);
 					pettxt4.setText(animal.getDescription());
 					break;
 				case 5:
-					petimg5.setImage(photoImg);
+					petimg5.setGraphic(photoImg);
 					pettxt5.setText(animal.getDescription());
 					break;
 				case 6:
-					petimg6.setImage(photoImg);
+					petimg6.setGraphic(photoImg);
 					pettxt6.setText(animal.getDescription());
 					break;
 				case 7:
-					petimg7.setImage(photoImg);
+					petimg7.setGraphic(photoImg);
 					pettxt7.setText(animal.getDescription());
 					break;
 				case 8:
-					petimg8.setImage(photoImg);
+					petimg8.setGraphic(photoImg);
 					pettxt8.setText(animal.getDescription());
 					break;
 				case 9:
-					petimg9.setImage(photoImg);
+					petimg9.setGraphic(photoImg);
 					pettxt9.setText(animal.getDescription());
 					break;
 					
@@ -130,43 +132,43 @@ public class MatchesPageController {
 
 				switch(slot){
 				case 0:
-					petimg0.setImage(null);
+					petimg0.setGraphic(null);
 					pettxt0.setText("");
 					break;
 				case 1:
-					petimg1.setImage(null);
+					petimg1.setGraphic(null);
 					pettxt1.setText("");
 					break;
 				case 2:
-					petimg2.setImage(null);
+					petimg2.setGraphic(null);
 					pettxt2.setText("");
 					break;
 				case 3:
-					petimg3.setImage(null);
+					petimg3.setGraphic(null);
 					pettxt3.setText("");
 					break;
 				case 4:		
-					petimg4.setImage(null);
+					petimg4.setGraphic(null);
 					pettxt4.setText("");
 					break;
 				case 5:
-					petimg5.setImage(null);
+					petimg5.setGraphic(null);
 					pettxt5.setText("");
 					break;
 				case 6:
-					petimg6.setImage(null);
+					petimg6.setGraphic(null);
 					pettxt6.setText("");
 					break;
 				case 7:
-					petimg7.setImage(null);
+					petimg7.setGraphic(null);
 					pettxt7.setText("");
 					break;
 				case 8:
-					petimg8.setImage(null);
+					petimg8.setGraphic(null);
 					pettxt8.setText("");
 					break;
 				case 9:
-					petimg9.setImage(null);
+					petimg9.setGraphic(null);
 					pettxt9.setText("");
 					break;
 					
@@ -190,47 +192,47 @@ public class MatchesPageController {
 				Animal animal = dbc.getAnimalById(id);
 				String photo = animal.getPhotosUrl()[0];
 				String text = animal.getDescription();
-				Image photoImg = new Image(photo);
+				ImageView photoImg = new ImageView(new Image(photo));
 				
 				switch(slot){
 				case 0:
-					petimg0.setImage(photoImg);
+					petimg0.setGraphic(photoImg);
 					pettxt0.setText(text);
 					break;
 				case 1:
-					petimg1.setImage(photoImg);
+					petimg1.setGraphic(photoImg);
 					pettxt1.setText(animal.getDescription());
 					break;
 				case 2:
-					petimg2.setImage(photoImg);
+					petimg2.setGraphic(photoImg);
 					pettxt2.setText(animal.getDescription());
 					break;
 				case 3:
-					petimg3.setImage(photoImg);
+					petimg3.setGraphic(photoImg);
 					pettxt3.setText(animal.getDescription());
 					break;
 				case 4:		
-					petimg4.setImage(photoImg);
+					petimg4.setGraphic(photoImg);
 					pettxt4.setText(animal.getDescription());
 					break;
 				case 5:
-					petimg5.setImage(photoImg);
+					petimg5.setGraphic(photoImg);
 					pettxt5.setText(animal.getDescription());
 					break;
 				case 6:
-					petimg6.setImage(photoImg);
+					petimg6.setGraphic(photoImg);
 					pettxt6.setText(animal.getDescription());
 					break;
 				case 7:
-					petimg7.setImage(photoImg);
+					petimg7.setGraphic(photoImg);
 					pettxt7.setText(animal.getDescription());
 					break;
 				case 8:
-					petimg8.setImage(photoImg);
+					petimg8.setGraphic(photoImg);
 					pettxt8.setText(animal.getDescription());
 					break;
 				case 9:
-					petimg9.setImage(photoImg);
+					petimg9.setGraphic(photoImg);
 					pettxt9.setText(animal.getDescription());
 					break;
 					
