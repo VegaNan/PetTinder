399a4a4ceb9038a5e4edaadc9dc5d50dce32fb91
Jacob Nuttall <jnuttall164@gmail.com>
2019-08-28 08:23:43 -0600

    Update SettingsController.java
    Unnecessary method is gone bye

--- Pinder/src/main/java/controllers/SettingsController.java
+++ Pinder/src/main/java/controllers/SettingsController.java
@@ -23,25 +23,6 @@ public class SettingsController {
 	private Stage primaryStage = GUI.primaryStage;
 	private static User currentUser;
 
-	private void changeScene(String filename) throws IOException {
-		// parent takes in the file
-		Parent parent = FXMLLoader.load(getClass().getResource(filename));
-		Scene scene = new Scene(parent);
-		Stage window = primaryStage;
-
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(filename));
-		SwipeyPageController controller = new SwipeyPageController();
-		loader.setController(controller);
-
-		System.out.println(controller);
-		controller.setPrimaryStage(primaryStage);
-
-		controller.setUser(currentUser);
-
-		window.setScene(scene);
-		window.show();
-	}
-
 	public void setUser(User user) {
 		currentUser = user;
 //		animalPrefLabel.setText("Your animal preference is "
